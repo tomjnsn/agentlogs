16bfe274b2d793bd2569758864053bf971019735
Philipp Spiess <hello@philippspiess.com>
2026-02-01 02:06:22 +0100

    e2e: Enable Playwright worker parallelism in CI

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: [["dot"], ["./server-log-reporter.ts"]],
 
   use: {
